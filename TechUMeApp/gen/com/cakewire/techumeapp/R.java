9169ce1a6a79669c6a6b21a386bd48066e98a0e0
Nico <domenico.gribbons@ocsbstudent.ca>
2013-12-03 09:35:50 -0500

    Testing the commit

--- TechUMeApp/gen/com/cakewire/techumeapp/R.java
+++ TechUMeApp/gen/com/cakewire/techumeapp/R.java
@@ -25,14 +25,18 @@ public final class R {
         public static final int sum=0x7f020001;
     }
     public static final class id {
-        public static final int Enter=0x7f080006;
-        public static final int Exit=0x7f080003;
-        public static final int Instutions=0x7f080002;
-        public static final int LinearLayout1=0x7f080004;
+        public static final int Enter=0x7f080005;
+        public static final int Instutions=0x7f080003;
         public static final int RelativeLayout1=0x7f080000;
         public static final int Use=0x7f080001;
-        public static final int action_settings=0x7f080007;
-        public static final int editText1=0x7f080005;
+        public static final int action_settings=0x7f08000b;
+        public static final int button1=0x7f080002;
+        public static final int editText1=0x7f080004;
+        public static final int radio0=0x7f080007;
+        public static final int radio1=0x7f080008;
+        public static final int radio2=0x7f080009;
+        public static final int radioButton1=0x7f08000a;
+        public static final int radioGroup1=0x7f080006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -42,14 +46,20 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int Convert=0x7f050008;
         public static final int Enter=0x7f050007;
         public static final int Exit=0x7f050005;
+        public static final int F2DAV=0x7f05000d;
+        public static final int F2theS=0x7f05000b;
         public static final int Insert=0x7f050006;
         public static final int Instrutions=0x7f050004;
         public static final int Use=0x7f050003;
+        public static final int V2f=0x7f05000c;
+        public static final int Vertex=0x7f050009;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int v2s=0x7f05000a;
     }
     public static final class style {
         /** 
