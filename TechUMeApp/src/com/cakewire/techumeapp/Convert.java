7e54648b46eaafa3c81625ace65464dfedf7c16d
Adonis <adonis.mansour@ocsbstudent.ca>
2013-12-10 09:48:14 -0500

    Changed the convert and secondscreen

---
+++ TechUMeApp/src/com/cakewire/techumeapp/Convert.java
@@ -0,0 +1,25 @@
+package com.cakewire.techumeapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class Convert extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_convert);
+		TextView convert = (TextView) findViewById(R.id.convert);
+		convert.setText(getIntent().getStringExtra(SecondScreen.EXTRA_MESSAGE));
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.convert, menu);
+		return true;
+	}
+
+}
--- TechUMeApp/src/com/cakewire/techumeapp/SecondScreen.java
+++ TechUMeApp/src/com/cakewire/techumeapp/SecondScreen.java
@@ -2,10 +2,16 @@ package com.cakewire.techumeapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class SecondScreen extends Activity {
 
+	public final static String EXTRA_MESSAGE = "com.cakewire.techumeapp.MESSAGE";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -17,6 +23,16 @@ public class SecondScreen extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.second_screen, menu);
 		return true;
+	}
+	
+	public void Calculate (View view) {
+		Intent intent = new Intent(this, Convert.class);
+		RadioGroup selectGroup = (RadioGroup)findViewById(R.id.radioGroup0);
+		int selected = selectGroup.getCheckedRadioButtonId();
+		RadioButton selectButton = (RadioButton)findViewById(selected);
+		String selectText = (String) selectButton.getText();
+		intent.putExtra(EXTRA_MESSAGE, selectText);
+		startActivity(intent);
 	}
 
 }
