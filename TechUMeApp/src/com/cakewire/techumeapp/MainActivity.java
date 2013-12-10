8e6e35dabc0c2a822375ffc77859df8d4a1e3b88
Samer Alsayegh <Samer@PvPKillz.com>
2013-12-10 09:26:37 -0500

    Made Rectangular buttons with a small radius, making it look much better. Lined up buttona so that both orientations of the screen view well

--- TechUMeApp/src/com/cakewire/techumeapp/MainActivity.java
+++ TechUMeApp/src/com/cakewire/techumeapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.cakewire.techumeapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -18,5 +20,18 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
+	public void switchToCalculations(View view){
+		Intent intent = new Intent(this, SecondScreen.class);
+		startActivity(intent);
+	}
+	
+	public void switchToInstructions(View view){
+		Intent intent = new Intent(this, Instructions.class);
+		startActivity(intent);
+	}
+	public void switchToAboutUs(View view){
+		Intent intent = new Intent(this, AboutUs.class);
+		startActivity(intent);
+	}
 }
