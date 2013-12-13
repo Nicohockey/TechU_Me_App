dbb5d905829dc44d9a5794c3577e21381fa4d3bc
Samer Alsayegh <Samer@PvPKillz.com>
2013-12-13 11:00:46 -0500

    Added conversion hide & show text methods Set-up the hide and show for factored and standard [Caveats] We need a layout to correctly show the fields without any issues....

--- TechUMeApp/src/com/cakewire/techumeapp/Convert.java
+++ TechUMeApp/src/com/cakewire/techumeapp/Convert.java
@@ -3,6 +3,8 @@ package com.cakewire.techumeapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Convert extends Activity {
@@ -13,8 +15,43 @@ public class Convert extends Activity {
 		setContentView(R.layout.activity_convert);
 		TextView convert = (TextView) findViewById(R.id.convert);
 		convert.setText(getIntent().getStringExtra(SecondScreen.EXTRA_MESSAGE));
+		String[] words = getIntent().getStringExtra(SecondScreen.EXTRA_MESSAGE).split(" ");
+		String option = words[0];
+		
+		if (option.equalsIgnoreCase("factored")){
+			hideText(R.id.numA_Field);
+			hideText(R.id.numB_Field);
+			hideText(R.id.numC_Field);
+			
+			showText(R.id.numX1_Field);
+			showText(R.id.numX1_Field);
+		}
+		else if (option.equalsIgnoreCase("standard")){
+			showText(R.id.numA_Field);
+			showText(R.id.numB_Field);
+			showText(R.id.numC_Field);
+			
+			hideText(R.id.numX1_Field);
+			hideText(R.id.numX1_Field);
+		}
+		else if (option.equalsIgnoreCase("vertex")){
+			
+			
+			
+		}
 	}
 
+	private void hideText(int numField) {
+		EditText text = (EditText) findViewById(numField);
+		text.setVisibility(View.GONE);
+	}
+	
+	private void showText(int numField) {
+		EditText text = (EditText) findViewById(numField);
+		text.setVisibility(View.VISIBLE);
+	}
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
